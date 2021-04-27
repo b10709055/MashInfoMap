f7bcf3dcdbdf61766fe14941221bd3de9b52d37a
adam <b10709055@gapps.ntust.edu.tw>
2021-04-28 00:05:18 +0800

    v2

--- app/src/main/java/com/adam/testmap/InfoWindowActivity.java
+++ app/src/main/java/com/adam/testmap/InfoWindowActivity.java
@@ -14,15 +14,23 @@ import com.google.android.gms.maps.model.Marker;
 public class InfoWindowActivity implements GoogleMap.InfoWindowAdapter {
 
     private Activity context;
-    private  String title="";
-    private  String subtitle="";
+
     public InfoWindowActivity(Activity context) {
         this.context = context;
     }
 
     @Override
     public View getInfoWindow(Marker marker) {
-        return null;
+        View view = context.getLayoutInflater().inflate(R.layout.activity_info_window,null,true);
+
+        TextView tvTitle = (TextView) view.findViewById(R.id.title);
+        TextView tvSubTitle = (TextView) view.findViewById(R.id.subtitle);
+
+        tvTitle.setText(marker.getTitle());
+        tvSubTitle.setText(marker.getSnippet());
+
+
+        return view;
     }
 
     @Override
@@ -32,9 +40,8 @@ public class InfoWindowActivity implements GoogleMap.InfoWindowAdapter {
         TextView tvTitle = (TextView) view.findViewById(R.id.title);
         TextView tvSubTitle = (TextView) view.findViewById(R.id.subtitle);
 
-        title=marker.getTitle();
-        System.out.println("------" + title + "--------");
-        tvTitle.setText(title);
+
+        tvTitle.setText(marker.getTitle());
         tvSubTitle.setText(marker.getSnippet());
 
 
